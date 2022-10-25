6828ad50fee63b95f4d57f44d25a419119680930
Kamil Łęga (Uczeń) <klega151@technikumkreatywne.pl>
2022-10-25 11:59:49 +0200

    update

--- index.php
+++ index.php
@@ -2,20 +2,29 @@
  <head>
     <title>Piwny Szczecin</title>
     <meta charset="utf-8">
-    <link rel="stylesheet" type="text/css" href="/styles.css">
-    <script src="/scripts.js"></script>
+    <link rel="stylesheet" type="text/css" href="styles.css">
+    <script src="scripts.js"></script>
  </head>
  <body>
-    <header>
-        <center>
-        <img src="">
-        <img src="">
-        </center>
-    </header>
-    <center>
+   <header>
+      <center>
+      zawartosc headera
+      </center>
+   </header>
+   <section>
+      <nav>
+         navbar
+      </nav>
 
-    </center>
+      <div id="content">   
+       damn
+       <img src="assets/photos/1st.png">
+      </div>
+   </section>
 
+   <footer>
+     footer
+   </footer>
 
  </body>
 </html>
